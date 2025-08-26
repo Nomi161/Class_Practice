81119992dbb70f9e18ebf4eadff55744d3176a17
HIRO-LENOVO\nomis <nomisan2010@gmail.com>
2025-08-26 15:39:39 +0900

    C# オブジェクト指向の練習

---
+++ Class_Practice/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    internal class Person
+    {
+        // 変数
+        protected string name;                // キャラの名前
+        protected int level;                  // レベル
+        protected int hp;                      // 体力
+        protected string info = "ただの村人";  // キャラ属性の特徴
+
+        // 実体化した際に基本的な情報は入るように設計
+        // 引数なしコンストラクタ
+        public Person()
+        {
+            name = "イッパンジーン";
+            level = 1;
+            hp = 100;
+        }
+
+        // 引数ありコンストラクタ
+        public Person(string n, int h)
+        {
+            name = n;
+            level = 1;
+            hp = h;
+        }
+
+        // 命令(メソッド)
+        // そのキャラの情報を表示するメソッド
+        public void InfoDisplay()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("名前：" + name);
+            Console.WriteLine("レベル：" + level);
+            Console.WriteLine("体力：" + hp);
+            Console.WriteLine(info);
+        }
+
+        // メンバ変数に干渉するためのメソッド達
+        // 名前を変更するメソッド
+        public void NameSet(string value)
+        {
+            name = value;
+        }
+
+        // HPを変更するメソッド
+        public void HPSet(int value)
+        {
+            hp = value;
+        }
+
+        // メンバ変数に干渉するためのプロパティ
+        // Nameという言い方ならnameに干渉できるように設計
+        public string Name
+        {
+            get { return name; }    // 読み取り機能 本来不干渉のnameの値をリターン(ゲット)します
+            set { name = value; }   // 書き込み機能 本来不干渉のnameに「Nameに代入された値」を代入
+        }
+
+        // 技をひとつ公表するメソッド
+        // オーバーロードして引数あり、引数なし 2種類作っておく
+        // 継承先でオーバーライドされるメソッドの予定なのでvirtual宣言も加えておく
+        public virtual void AttackInfo(int x)
+        {
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("技：必殺の右ストレート");
+                    break;
+                default:
+                    Console.WriteLine("技：渾身のタックル");
+                    break;
+            }
+        }
+
+        public virtual void AttackInfo()
+        {
+            Console.WriteLine("技：渾身のタックル");
+        }
+    }
+}
---
+++ Class_Practice/Warrior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    internal class Warrior : Person
+    {
+        // コンストラクタ
+        public Warrior(string n, int h)
+        {
+            name = n;
+            hp = h;
+            info = "強い剣士";
+        }
+
+        // コンストラクタ(引数なし)
+        public Warrior() : this("タンジーロ", 500) { }
+
+        // 継承したメソッドをオーバーライド(上書き)して違う機能に書き換える
+        public override void AttackInfo(int x)
+        {
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("技：縦斬り");
+                    break;
+                case 2:
+                    Console.WriteLine("技：横斬");
+                    break;
+                case 3:
+                    Console.WriteLine("技：連続斬り");
+                    break;
+                default:
+                    Console.WriteLine("みねうち");
+                    break;
+            }
+        }
+
+        // 引数なしメソッドも継承して上書きするオーバーライド
+        public override void AttackInfo()
+        {
+            Console.WriteLine("技：みねうち");
+        }
+
+        // まったく新しい独自の命令(戻り値ありメソッド)
+        public string Special()
+        {
+            string message = "家族を絶対に守る！";
+            return message; // 変数messageの中身を戻す
+        }
+    }
+}
